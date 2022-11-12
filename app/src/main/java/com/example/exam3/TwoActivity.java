350294908822135ba12e071fcac1b1a7b0bce8e0
guo061 <1466718529@qq.com>
2022-11-12 11:46:40 +0800

    submit exam3

---
+++ app/src/main/java/com/example/exam3/TwoActivity.java
@@ -0,0 +1,14 @@
+package com.example.exam3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.support.v7.app.AppCompatActivity;
+
+public class TwoActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main5);
+    }
+}
